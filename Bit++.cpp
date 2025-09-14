f8b2fd17a3f6bb3afd79b0a9a98aa09cb315371f
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2025-09-14 20:01:34 +0600

    Create Bit++.cpp

---
+++ Bit++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int x = 0;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;  // Read the statement
+        if (s.find("++") != string::npos) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+
+    cout << x << endl;
+    return 0;
+}
